75a23368bc579890b5678698fd7c18cff2fbe240
jsmolka <jsmolka@pm.me>
2017-10-23 20:19:46 +0200

    formatting

--- sfml/particle_system/ParticleSystem.hpp
+++ sfml/particle_system/ParticleSystem.hpp
@@ -7,23 +7,17 @@
 class ParticleSystem : public sf::Drawable, public sf::Transformable
 {
 public:
-
     explicit ParticleSystem(unsigned int count);
-
     void setEmitter(sf::Vector2f position);
-
     void update(sf::Time elapsed);
 
 private:
-
     void draw(sf::RenderTarget& target, sf::RenderStates states) const override;
-
     struct Particle
     {
         sf::Vector2f velocity;
         sf::Time lifetime;
     };
-
     void resetParticle(std::size_t index);
 
     std::vector<Particle> m_particles;
