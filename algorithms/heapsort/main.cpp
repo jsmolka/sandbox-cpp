614307fc1d3754e8cfdf057db9f181abb6046289
jsmolka <jsmolka@pm.me>
2017-11-28 10:31:58 +0100

    finished version

--- algorithms/heapsort/main.cpp
+++ algorithms/heapsort/main.cpp
@@ -1,54 +1,56 @@
 #include <iostream>
 
-// https://gist.github.com/mopkaloppt/63108d67fed6d3161ad9
-
-void printHeap(int* heap, int size)
+void print(int* array, int size)  // O(n)
 {
     for(int i = 0; i < size; i++)
-        std::cout << heap[i] << " ";
+        std::cout << array[i] << " ";
     std::cout << std::endl;
 }
 
-void swapDown(int* heap, int i, int size)
+void heapify(int* heap, int size, int root)  // O(log n)
 {
-    int left = 2 * i + 1;
-    int right = 2 * i + 2;
-    int min = i;
-    // find who holds smallest element of i and its two children
+    int min = root;
+    // Calculate root's left and right children
+    int left = 2 * root + 1;
+    int right = 2 * root + 2;
+    // Find out if and which of root's two children holds the minimal value
     if(left < size && heap[left] < heap[min])
+        // Assign children index to min if it is smaller
         min = left;
     if(right < size && heap[right] < heap[min])
         min = right;
-    // if a child holds smallest element, swap i's element to that child
-    // and recurse.
-    if(min != i)
+    if(min != root)
     {
-        std::swap(heap[i], heap[min]);
-        swapDown(heap, min, size);
+        // Swap root with min if they are unequal
+        std::swap(heap[root], heap[min]);
+        // Recursively swap root and child until tree has a heap structure
+        heapify(heap, size, min);
     }
 }
 
-void heapify(int* heap, int size)
+void build(int* heap, int size)  // O(n)
 {
-    for (int i = (size - 2) / 2; i >= 0; i--)
-        swapDown(heap, i, size);
+    // Lowest level with leaves begins at size / 2 - 1
+    // Just go through levels without leaves and heapify them from lowest to highest
+    for (int root = size / 2 - 1; root >= 0; root--)
+        heapify(heap, size, root);
 }
 
-int find(int* heap, int size, int value, int root=0)
+int find(int* heap, int size, int value, int root=0)  // O(n)
 {
     // Check if root is the searched value
     if (heap[root] == value)
         return root;
-    // Calculate index of roots left children
+    // Calculate index of root's left children
     int left = 2 * root + 1;
     // Check if the calculated index is out of bounds
-    // Compare the heap value of the left children to the searched value
-    // If the heap value equals the searched value, the function will return at the next iteration
-    // If the heap value is smaller than the searched value, it might occur in later iterations of this sub part
+    // Compare the heap's value of the left children to the searched value
+    // If the heap's value equals the searched value, the function will return at the next iteration
+    // If the heap's value is smaller than the searched value, it might occur in later iterations of this sub part
     // Else the whole sub part can be skipped because there is no chance of finding the searched value
     if (left < size && heap[left] <= value)
     {
-        std::cout << "root " << root << " left " << left << " heap " << heap[left] << std::endl;
+        std::cout << "root " << heap[root] << " left " << heap[left] << std::endl;
         // Call the function with the left children as root and search through the sub part
         int result = find(heap, size, value, left);
         // Return the searched index if it was found
@@ -59,7 +61,7 @@ int find(int* heap, int size, int value, int root=0)
     int right = 2 * root + 2;
     if (right < size && heap[right] <= value)
     {
-        std::cout << "root " << root << " right " << right << " heap " << heap[right] << std::endl;
+        std::cout << "root " << heap[root] << " right " << heap[right] << std::endl;
         int result = find(heap, size, value, right);
         if (result != -1)
             return result;
@@ -68,39 +70,50 @@ int find(int* heap, int size, int value, int root=0)
     return -1;
 }
 
-void sort(int* heap, int size)
+void sort(int* heap, int size)  // O(n log n)
 {
-    for (int i = size - 1; i > 0; i--)
+    // Make sure array has heap structure
+    build(heap, size);
+    // Build creates a min heap -> sort in descending order
+    for (int root = size - 1; root > 0; root--)
     {
-        std::swap(heap[0], heap[i]);
-        swapDown(heap, 0, i);
+        // Smallest element is first, swap it with the last
+        std::swap(heap[0], heap[root]);
+        // Make sure the second smallest element is at first position
+        heapify(heap, root, 0);
     }
-}
-
-// TODO: Remove before handing in
-void heapStructure(int *heap, int size, int node=0, int d=0)
-{
-    int left = (2 * node) + 1;
-    int right = left + 1;
-
-    if (right < size)
-        heapStructure(heap, size, right, d + 3);
-
-    std::cout << std::string(d, ' ') + std::to_string(heap[node]) << std::endl;
-
-    if (left < size)
-        heapStructure(heap, size, left, d + 3);
 }
 
 int main()
 {
-    int heap[12] = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0, 12};
-    heapify(heap, 12);
-    heapStructure(heap, 12);
-    printHeap(heap, 12);
-    int m = find(heap, 12, 12);
-    std::cout << "result: " << m << std::endl;
+    int heap[9] = {6, 2, 3, 8, 7, 5, 10, 12, 1};
+    std::cout << "array: ";
+    print(heap, 9);
 
+    // 4.
+    build(heap, 9);
+    std::cout << "heap: ";
+    print(heap, 9);
+
+    // 6.
+    std::cout << "start searching 10" << std::endl;
+    int result = find(heap, 9, 10);
+    if (result != -1)
+        std::cout << "value 10 found at index " << result << std::endl;
+    else
+        std::cout << "value 10 not found" << std::endl;
+
+    // 7.
+    std::cout << "start searching 34" << std::endl;
+    result = find(heap, 9, 34);
+    if (result != -1)
+        std::cout << "value 34 found at index " << result << std::endl;
+    else
+        std::cout << "value 34 not found" << std::endl;
+
+    // 5.
+    sort(heap, 9);
+    std::cout << "sorted: "; print(heap, 9);
 
     return 0;
 }
