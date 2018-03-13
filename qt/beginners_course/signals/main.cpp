029e36d37f5597233f995ef1f5933b7308bce5f4
jsmolka <jsmolka@pm.me>
2018-03-13 13:02:03 +0100

    add signals example

---
+++ qt/beginners_course/signals/main.cpp
@@ -0,0 +1,29 @@
+#include <QApplication>
+#include <QProgressBar>
+#include <QSlider>
+
+int main(int argc, char *argv[])
+{
+    QApplication app(argc, argv);
+
+    // Create a container window
+    QWidget window; window.setFixedSize(480, 110);
+
+    // Create a progress bar
+    QProgressBar *progressBar = new QProgressBar(&window);
+    progressBar->setRange(0, 100); progressBar->setValue(0); progressBar->setGeometry(10, 10, 460, 40);
+
+    // Create horizontal slider
+    QSlider *slider = new QSlider(&window);
+    slider->setOrientation(Qt::Horizontal);
+    slider->setRange(0, 100);
+    slider->setValue(0);
+    slider->setGeometry(10, 60, 460, 40);
+
+    window.show();
+
+    // Create connection
+    QObject::connect(slider, SIGNAL(valueChanged(int)), progressBar, SLOT(setValue(int)));
+
+    return app.exec();
+}
