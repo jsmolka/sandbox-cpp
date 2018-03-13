c425b1868623e03f676db9e06f5480b9f977ac44
jsmolka <jsmolka@pm.me>
2018-03-13 10:49:55 +0100

    add property

--- qt/beginners_course/hello_world/main.cpp
+++ qt/beginners_course/hello_world/main.cpp
@@ -8,7 +8,8 @@ int main(int argc, char *argv[])
     QPushButton button ("Hello world!");
     button.setText("My Text");
     button.setToolTip("A tooltip");
-    button.setFont(QFont ("Courier"));
+    button.setFont(QFont("Courier"));
+    button.setFixedSize(480, 360);
     button.show();
 
     return app.exec();
